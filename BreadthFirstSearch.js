d99e29846b205da62d0d9d96070d04a56d740fdf
Sumit Patel <sumitz1207@gmail.com>
2023-03-12 18:11:03 -0400

    Update BreadthFirstSearch.js

--- BreadthFirstSearch.js
+++ BreadthFirstSearch.js
@@ -1,1 +1,15 @@
 
+const breadthFirstTraversal = (root) => {
+  if (root === null) return [];
+
+  const result = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    result.push(current.data);
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return result;
+};
