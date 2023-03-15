cc05a7fbcc28b0ef36399ea898905ad74c3d3b0b
Sumit Patel <sumitz1207@gmail.com>
2023-03-14 20:14:35 -0400

    Update MergeSort.js

--- MergeSort.js
+++ MergeSort.js
@@ -1,1 +1,35 @@
 
+function mergeSort (unsortedArray) {
+  if (unsortedArray.length <= 1) {
+    return unsortedArray;
+  }
+  
+  const middle = Math.floor(unsortedArray.length / 2);
+
+  const left = unsortedArray.slice(0, middle);
+  const right = unsortedArray.slice(middle);
+
+  // Using recursion to combine the left and right
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  );
+}
+
+function merge (left, right) {
+  let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+	  rightIndex++;
+    }
+  }
+
+  return resultArray
+          .concat(left.slice(leftIndex))
+          .concat(right.slice(rightIndex));
+}
